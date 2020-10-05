8f046f92c3dd9619f5b99608ae4970626a9ff2fb
kfiorino <63323923+kfiorino@users.noreply.github.com>
2020-10-05 10:08:59 -0400

    init push

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,11 @@
+import {gsap} from "gsap";
 
+import {section1Animation} from "./sections/section-1.js"
+import {section2Animation} from "./sections/section-2.js"
+import {section3Animation} from "./sections/section-3.js"
+import {section4Animation} from "./sections/section-4.js"
+
+section1Animation();
+section2Animation();
+section3Animation();
+section4Animation();
---
+++ dev/js/section-1.js
@@ -0,0 +1,5 @@
+import {gsap} from "gsap";
+
+export function section1Animation(){
+    gsap.from("#section-1 div", {duration:1, alpha:0, y:400});
+}
